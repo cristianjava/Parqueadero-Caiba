99f24f093e9bdbd5c40397367f27d901e9418d22
Cristian David Castaño Giraldo - Ceiba Software <cristian.castano@ceiba.com.co>
2018-07-30 09:23:03 -0500

    Se sube commit inicial con arquitectura en gradle

---
+++ src/main/java/ejemploTres/ejemploTres/EjemploTresApplication.java
@@ -0,0 +1,12 @@
+package ejemploTres.ejemploTres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploTresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploTresApplication.class, args);
+	}
+}
---
+++ src/test/java/ejemploTres/ejemploTres/EjemploTresApplicationTests.java
@@ -0,0 +1,16 @@
+package ejemploTres.ejemploTres;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EjemploTresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
